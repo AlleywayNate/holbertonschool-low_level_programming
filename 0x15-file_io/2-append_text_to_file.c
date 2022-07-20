9c25cd8f9e95d97d53e9156a0eab519071bf1f39
AlleywayNate <4937@holbertonschool.com>
2022-07-19 21:18:20 -0700

    Finished

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,37 +1,54 @@
 #include "main.h"
 
 /**
-  * length_str - function that creates a file.
-  * @string: string
-  * Return: get length of string
-  */
-size_t length_str(const char *string)
+ * _strlen - find length of string
+ * @str: string
+ * Return: length
+ */
+int _strlen(char *str)
 {
-	return ((*string == '\0') ? 0 : 1 + length_str(string + 1));
+	int len;
+
+	for (len = 0; str[len] != '\0'; len++)
+		;
+
+	return (len);
 }
+
 /**
-* append_text_to_file - function that appends text at the end of a file.
-* @filename: filename
-* @text_content: text_content
-* Return: size
-*/
+ * append_text_to_file - append text and only if file exists
+ * @filename: file
+ * @text_content: appends this content into file
+ * Return: 1 on success, -1 on error
+ */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int file_id = 0;
-	size_t size;
+	int fd;
+	int n_wrote;
 
 	if (!filename)
-	return (-1);
-
-	file_id = open(filename, O_APPEND | O_WRONLY, 0600);
-	if (file_id == -1)
 		return (-1);
 
-	size = length_str(text_content);
-
-	if (write(file_id, text_content, size) == -1)
+	/* open file if it exists */
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
 		return (-1);
 
-	close(file_d);
+	/* if nothing to write, still successful */
+	if (!text_content)
+	{
+		close(fd);
+		return (1);
+	}
+
+	/* write */
+	n_wrote = write(fd, text_content, _strlen(text_content));
+	if (n_wrote == -1 || n_wrote != _strlen(text_content))
+	{
+		close(fd);
+		return (-1);
+	}
+
+	close(fd);
 	return (1);
 }
