300b7cbcf0df1bac38d5105cb08a727a736de8e7
AlleywayNate <4937@holbertonschool.com>
2022-07-19 21:13:55 -0700

    Finished

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,33 +1,37 @@
 #include "main.h"
 
 /**
- * append_text_to_file - program startup
- * @filename: text file
- * @text_content: the NULL terminated string to add at the end of the file
-(* Description: appends text at the end of a file)?
- * Return: (1) if the file exists and (-1) if the file does not exist
- */
-
+  * length_str - function that creates a file.
+  * @string: string
+  * Return: get length of string
+  */
+size_t length_str(const char *string)
+{
+	return ((*string == '\0') ? 0 : 1 + length_str(string + 1));
+}
+/**
+* append_text_to_file - function that appends text at the end of a file.
+* @filename: filename
+* @text_content: text_content
+* Return: size
+*/
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int o, w, len = 0;
+	int file_id = 0;
+	size_t size;
 
-	if (filename == NULL)
-	{
-		return (-1);
-	}
-	if (text_content != NULL)
-	{
-		for (len = 0; text_content[len];)
-			len++;
-	}
-	o = open(filename, O_WRONLY | O_APPEND);
-	w = write(o, text_content, len);
-	if (o == -1 || w == -1)
-	{
-		return (-1);
-	}
-	close(o);
+	if (!filename)
+	return (-1);
 
+	file_id = open(filename, O_APPEND | O_WRONLY, 0600);
+	if (file_id == -1)
+		return (-1);
+
+	size = length_str(text_content);
+
+	if (write(file_id, text_content, size) == -1)
+		return (-1);
+
+	close(file_d);
 	return (1);
 }
